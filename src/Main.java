719531fb4408653a4f187baf0e844e8b575ea1dd
ryanliupie <ryanliu314@gmail.com>
2025-04-09 09:58:54 -0400

    first program of java

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+// first time writing java 
+
+public class Main { // "public" refers to the class being accessible from other classes and every file can only have 1 public class. It must also match the file name where "Main" refers to "Main.java". If you do not want this class being accessed from anywhere, you can just use "class Main".  
+
+    public static void main(String[] args){ // this "main method" is REQUIRED!! in order for the java program to run
+    
+        System.out.println("I love the RS6 and the X5M."); // you can use "println" to separate texts
+        System.out.print("\nQuizlet helps a lot during exams."); // or you can use the escape sequence "\n" that we learnt in python to separate the texts  
+        System.out.print("\nCopying word for word is a waste of time.");
+    }
+}
