9d76e12c1316dba0a47c210968dd501926b7518e
ryanliupie <ryanliu314@gmail.com>
2025-04-16 11:02:53 -0400

    minor fixes

--- src/simple_programs/Hypotenuse.java
+++ src/simple_programs/Hypotenuse.java
@@ -8,17 +8,17 @@ public class Hypotenuse {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
 
-            System.out.print("Enter Side A: "); 
+            System.out.print("Enter the length of side A: "); 
             double a = scanner.nextDouble();
 
-            System.out.print("Enter side B: "); 
+            System.out.print("Enter the length of side B: "); 
             double b = scanner.nextDouble();
 
             double result; 
             
             result = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2)); 
 
-            System.out.print("The hypotenuse of the triangle is " + String.format("%.2f", result) + "m^2"); 
+            System.out.print("The hypotenuse of the triangle is " + String.format("%.2f", result) + "m"); 
         }
 
     }
